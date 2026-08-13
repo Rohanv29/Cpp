0b2a93d65781c6004dba818a11451d8ba0b1ca6b
2k25csaiml2513533-gif <2k25aiml2513533@gmail.com>
2026-08-13 16:49:32 +0530

    without extra space

--- dp/fibo.cpp
+++ dp/fibo.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
-#include <vector>
 using namespace std;
-int fib(int n,vector<int>&dp){
-    if(n<=1) return n;
-    if(dp[n]!=-1) return dp[n];
-    return dp[n]=fib(n-1,dp)+fib(n-2,dp);
-}
+// int fib(int n,vector<int>&dp){
+//     if(n<=1) return n;
+//     if(dp[n]!=-1) return dp[n];
+//     return dp[n]=fib(n-1,dp)+fib(n-2,dp);
+// }
 int main(){
     int n;
     cin>>n;
-    vector<int>dp(n+1,-1);
-    cout<<fib(n,dp);
+    if(n<=1) return n;
+    int prev2=0,prev=1;
+    for(int i=2;i<=n;i++){
+        int curr=prev+prev2;
+        prev2=prev;
+        prev=curr;
+    }
+    cout<<prev;
+    // vector<int>dp(n+1,-1);
+    // cout<<fib(n,dp);
     return 0;
 }
