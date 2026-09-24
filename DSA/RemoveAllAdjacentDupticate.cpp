52371b7b049fd2fe8eeca2e1bc97e19533f57d20
2k25csaiml2513533-gif <2k25aiml2513533@gmail.com>
2026-09-24 12:32:41 +0530

    remove adjacent Duplicate

---
+++ DSA/RemoveAllAdjacentDupticate.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+#include<stack>
+using namespace std;
+int main(){
+    string s="abbaca";
+    string ans;
+    for(char ch:s){
+        if(!ans.empty() && ans.back()==ch){
+            ans.pop_back();
+        } 
+        else{
+            ans.push_back(ch);
+        }
+    }
+    cout<<ans;
+    return 0;
+}
